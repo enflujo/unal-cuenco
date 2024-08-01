fb621548c041a1b86af3d2e420381d071065b958
anattolia <anattolia@gmail.com>
2024-08-01 18:00:54 -0500

    🎉 crear cerebro

---
+++ aplicaciones/www/src/utilidades/cerebro.ts
@@ -0,0 +1,12 @@
+import { atom, map } from 'nanostores';
+import type { Listas } from '@/tipos';
+
+export const nombresListasProyectos = {
+  publicacion: 'Publicacion',
+  autores: 'Autores',
+  tipos: 'Tipos',
+  años: 'Años',
+  dependencias: 'Dependencias',
+  indicadores: 'Indicadores',
+  subindicadores: 'Subindicadores',
+};
