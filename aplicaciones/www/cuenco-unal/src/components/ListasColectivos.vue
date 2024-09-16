e358d52f442e536b3b7477d63a64c540ffd14773
anattolia <anattolia@gmail.com>
2024-09-16 10:08:03 -0500

    ✨ Mostrar Colectivos en vue

---
+++ aplicaciones/www/cuenco-unal/src/components/ListasColectivos.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import ListaNodos from '../components/ListaNodos.vue'
+import listasColectivos from '../../public/datos/listasColectivos.json'
+
+defineProps<{}>()
+</script>
+
+<template>
+  <div id="contenedorListas" class="todoVisible">
+    <ListaNodos id="años" :lista="listasColectivos.años" />
+    <ListaNodos id="estados" :lista="listasColectivos.estados" />
+    <ListaNodos id="responsables" :lista="listasColectivos.responsables" />
+    <ListaNodos id="sedes" :lista="listasColectivos.sedes" />
+    <ListaNodos id="dependencias" :lista="listasColectivos.dependencias" />
+    <ListaNodos id="modalidades" :lista="listasColectivos.modalidades" />
+    <ListaNodos id="indicadores" :lista="listasColectivos.indicadores" />
+    <ListaNodos id="tipos" :lista="listasColectivos.tipos" />
+  </div>
+</template>
+
+<style scoped></style>
--- aplicaciones/www/cuenco-unal/src/views/VistaColectivos.vue
+++ aplicaciones/www/cuenco-unal/src/views/VistaColectivos.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ListasColectivos from '@/components/ListasColectivos.vue'
+</script>
 
 <template>
   <div>
     <h1>Colectivos y Ámbitos</h1>
-    <p></p>
+    <ListasColectivos />
   </div>
 </template>
 
