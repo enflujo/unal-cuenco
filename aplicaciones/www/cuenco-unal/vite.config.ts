64ce139b4c43f6274b8777fd65e6542c53bb461a
anattolia <anattolia@gmail.com>
2024-09-16 14:31:46 -0500

    🚧 limpiar configuraciones y package json

--- aplicaciones/www/cuenco-unal/vite.config.ts
+++ aplicaciones/www/cuenco-unal/vite.config.ts
@@ -1,16 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  build: {
+    outDir: 'publico',
+    assetsDir: 'estaticos',
+    sourcemap: true,
+  },
+  publicDir: 'estaticos',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
