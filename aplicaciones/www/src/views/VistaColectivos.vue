561f60400c0934ec77a53fa2bf2c9c1d30e1f3d4
anattolia <anattolia@gmail.com>
2024-10-21 18:09:20 -0500

    ✨ Botones para cambiar vistas

--- aplicaciones/www/src/views/VistaColectivos.vue
+++ aplicaciones/www/src/views/VistaColectivos.vue
@@ -2,15 +2,52 @@
 import ComponenteMapa from '@/components/ComponenteMapa.vue';
 import FichaColectivosAmbitos from '@/components/FichaColectivosAmbitos.vue';
 import ListasColectivos from '@/components/ListasColectivos.vue';
+import VistaGraficas from '@/components/VistaGraficas.vue';
+import { ref, type Ref } from 'vue';
+
+const vista: Ref<string> = ref('');
+const botonGraficas: Ref<HTMLDivElement | undefined> = ref();
+const botonMapa: Ref<HTMLDivElement | undefined> = ref();
+
+function elegirVista(vistaElegida: string) {
+  vista.value = vistaElegida;
+}
 </script>
 
 <template>
   <div>
-    <h1>Colectivos y Ámbitos</h1>
+    <h1 class="tituloSeccion">Colectivos y Ámbitos</h1>
+    <div class="botonesVista">
+      <div class="botonVista" ref="botonGraficas" @click="elegirVista('grafica')">Gráficas</div>
+      <div class="botonVista" ref="botonMapa" @click="elegirVista('mapa')">Mapa</div>
+    </div>
     <ListasColectivos />
-    <ComponenteMapa />
-    <FichaColectivosAmbitos id="2" />
+
+    <div v-if="vista === 'grafica'">
+      <VistaGraficas />
+    </div>
+
+    <div v-else="vista === 'mapa'">
+      <ComponenteMapa />
+    </div>
+
+    <!-- <FichaColectivosAmbitos id="2" /> -->
   </div>
 </template>
 
-<style></style>
+<style>
+.botonesVista {
+  position: relative;
+  left: 20vw;
+  display: flex;
+  justify-content: flex-start;
+  width: 20vw;
+}
+.tituloSeccion {
+  text-align: center;
+}
+.botonVista {
+  margin: 1em;
+  cursor: pointer;
+}
+</style>
--- aplicaciones/www/src/views/VistaPublicaciones.vue
+++ aplicaciones/www/src/views/VistaPublicaciones.vue
@@ -4,17 +4,52 @@ import LineaTiempo from '../components/LineaTiempo.vue';
 import ListasPublicaciones from '../components/ListasPublicaciones.vue';
 import FichaProduccionAcademica from '@/components/FichaProduccionAcademica.vue';
 import VistaGraficas from '@/components/VistaGraficas.vue';
+import { ref, type Ref } from 'vue';
+
+const vista: Ref<string> = ref('');
+const botonGraficas: Ref<HTMLDivElement | undefined> = ref();
+const botonMapa: Ref<HTMLDivElement | undefined> = ref();
+
+function elegirVista(vistaElegida: string) {
+  vista.value = vistaElegida;
+}
 </script>
 
 <template>
   <div>
-    <h1>Producción Académica</h1>
+    <h1 class="tituloSeccion">Producción Académica</h1>
+    <div class="botonesVista">
+      <div class="botonVista" ref="botonGraficas" @click="elegirVista('grafica')">Gráficas</div>
+      <div class="botonVista" ref="botonMapa" @click="elegirVista('mapa')">Mapa</div>
+    </div>
     <ListasPublicaciones />
-    <!--  <ComponenteMapa /> -->
-    <VistaGraficas />
+
+    <div v-if="vista === 'grafica'">
+      <VistaGraficas />
+    </div>
+
+    <div v-else="vista === 'mapa'">
+      <ComponenteMapa />
+    </div>
+
     <LineaTiempo />
     <!--  <FichaProduccionAcademica /> -->
   </div>
 </template>
 
-<style></style>
+<style>
+.botonesVista {
+  position: relative;
+  left: 20vw;
+  display: flex;
+  justify-content: flex-start;
+  width: 20vw;
+}
+.tituloSeccion {
+  text-align: center;
+}
+.botonVista {
+  margin: 1em;
+  cursor: pointer;
+}
+</style>
