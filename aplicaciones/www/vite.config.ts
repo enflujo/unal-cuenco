aa7cbff0ee6d48f7392f509578533dca945c3e2a
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2024-11-09 15:39:18 -0500

    🚀 Vite exporta un archivo 404.html con redirección para que funcionen las rutas.

--- aplicaciones/www/vite.config.ts
+++ aplicaciones/www/vite.config.ts
@@ -2,11 +2,21 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/unal-cuenco/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '404.html',
+          dest: '',
+        },
+      ],
+    }),
+  ],
   build: {
     outDir: 'publico',
     assetsDir: 'estaticos',
