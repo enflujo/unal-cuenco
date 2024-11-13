f4047e911b62181e897f611eef8d1531dfa5b4fd
anattolia <anattolia@gmail.com>
2024-11-13 17:50:29 -0500

    🚧 vista gráficas fitradas

--- aplicaciones/www/src/componentes/VistaGraficas.vue
+++ aplicaciones/www/src/componentes/VistaGraficas.vue
@@ -22,6 +22,7 @@ const listaVisible: Ref<ElementoLista[] | null> = ref(null);
 const valorMaximo = ref(0);
 const listas: Ref<ListasColectivos | ListasPublicaciones | null> = ref(null);
 const listaActual: Ref<LlavesColectivos | LlavesPublicaciones | null> = ref(null);
+const etiquetaCortada: Ref<HTMLDivElement | null> = ref(null);
 const listaFiltros = computed<{ llave: LlavesColectivos | LlavesPublicaciones; nombre: string }[] | null>(() => {
   if (listas.value) {
     return (Object.keys(listas.value) as (LlavesColectivos | LlavesPublicaciones)[])
@@ -32,8 +33,37 @@ const listaFiltros = computed<{ llave: LlavesColectivos | LlavesPublicaciones; n
 });
 
 let filtrados: Relacion[][] = [];
-
 const filtroElegido: Ref<string> = ref('sedes');
+let posicionIzq = 0;
+
+// Colores para dividir las barras
+const colores: string[] = [
+  '#3949a4',
+  '#c30a93',
+  '#2B2D42',
+  '#8D99AE',
+  '#EE8B98',
+  '#EF233C',
+  '#45CB85',
+  '#22AED1',
+  '#773344',
+  '#d8cc34',
+  '#edc9ff',
+  '#6eeb83',
+  '#e8aa14',
+  '#3bf4fb',
+  '#ff5714',
+  '#1a1b41',
+  '#c2e7da',
+  '#a4243b',
+  '#c879ff',
+  '#caff8a',
+  '#363457',
+  '#735290',
+  '#465c69',
+  '#C4A69D',
+  '#240046',
+];
 
 watch(listaElegida, (llaveLista) => {
   if (!llaveLista || llaveLista === listaActual.value) return;
@@ -71,16 +101,24 @@ onUnmounted(() => {
 });
 
 function elegirFiltro(filtro: string) {
+  filtroElegido.value = filtro;
   if (listaActual.value === filtro || !listaVisible.value) return;
   filtrados = [];
   listaVisible.value.forEach((elementoElegido) => {
     const filtrado = elementoElegido.relaciones.filter((relacion) => relacion.tipo === `${filtroElegido.value}`);
-
     filtrado.sort((a, b) => b.conteo - a.conteo);
+
     filtrados.push(filtrado);
   });
+}
 
-  filtroElegido.value = filtro;
+function mouseEnter({ target, clientX, clientY }: MouseEvent) {
+  const elemento = target as HTMLElement;
+  if (!etiquetaCortada.value || !elemento.dataset.conteo) return;
+  etiquetaCortada.value.innerText = elemento.dataset.conteo;
+  etiquetaCortada.value.style.left = `${clientX - 390}px`;
+  etiquetaCortada.value.style.top = `${clientY - 210}px`;
+  etiquetaCortada.value.style.display = 'block';
 }
 </script>
 
@@ -102,14 +140,30 @@ function elegirFiltro(filtro: string) {
       </div>
     </div>
 
-    <!-- Mostrar filtrados -->
-    <div v-if="listaVisible" v-for="(elementos, i) in filtrados">
-      {{ listaVisible[i].nombre }}
-      <div v-for="e in elementos">{{ e.id }}: {{ e.conteo }}</div>
-    </div>
-
     <div id="contenedorGrafica" v-if="listaVisible">
-      <div class="contenedorElementos" v-for="(elem, i) in listaVisible">
+      <!-- Mostrar filtrados -->
+      <div v-if="listaVisible" v-for="(elementos, j) in filtrados" :v-on="(posicionIzq = 0)">
+        <p class="etiquetaFiltro" :style="`top: ${j * 4}%;`">{{ listaVisible[j].nombre }}</p>
+
+        <div v-for="(e, i) in elementos" class="contenedorElementos">
+          <div
+            @mouseenter="mouseEnter"
+            class="lineaCortada"
+            :v-on="
+              i === 0
+                ? (posicionIzq = 0)
+                : (posicionIzq += convertirEscala(elementos[i - 1].conteo, 0, valorMaximo, 0, 70))
+            "
+            :style="`width:${convertirEscala(e.conteo, 0, valorMaximo, 0, 70)}%; top: ${j * 4}%; left: ${i === 0 ? 0 : posicionIzq}%; background-color:${i < colores.length ? colores[i] : colores[0]}`"
+            :data-conteo="`${e.conteo}`"
+            :data-id="`${e.id}`"
+          ></div>
+        </div>
+      </div>
+      <div class="etiquetaCortada" ref="etiquetaCortada"></div>
+
+      <!--Líneas no filtradas-->
+      <div v-if="!filtrados.length" class="contenedorElementos" v-for="(elem, i) in listaVisible">
         <p class="leyenda" :style="`top:${convertirEscala(i, 0, listaVisible.length, 0, 70)}%`">
           {{ elem.nombre }}
         </p>
@@ -152,12 +206,21 @@ function elegirFiltro(filtro: string) {
   background-color: rgb(255, 255, 255);
   width: 100%;
   content-visibility: auto;
+  height: fit-content;
+  padding-bottom: 25em;
+  margin-bottom: 100px;
 
   .contenedorElementos {
     display: flex;
     justify-content: flex-start;
     align-items: center;
     margin-bottom: 0.5em;
+  }
+  .etiquetaCortada {
+    display: none;
+    position: absolute;
+    background-color: white;
+    padding: 0.2em;
   }
 }
 
@@ -170,6 +233,17 @@ function elegirFiltro(filtro: string) {
 .linea {
   height: 1px;
   background-color: var(--azulOscuroCuenco);
+}
+
+.etiquetaFiltro {
+  position: absolute;
+  left: 0%;
+}
+
+.lineaCortada {
+  position: absolute;
+  height: 8px;
+  margin-top: 2em;
 }
 
 .colombino {
