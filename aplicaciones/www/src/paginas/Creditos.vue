5b70ba3d6858bd447d798f36da6b436c297dafe1
anattolia <anattolia@gmail.com>
2025-06-11 16:51:45 -0500

    📝 quitar créditos

--- aplicaciones/www/src/paginas/Creditos.vue
+++ aplicaciones/www/src/paginas/Creditos.vue
@@ -20,25 +20,14 @@ onUnmounted(() => {
     <div id="contenedorCreditos">
       <h1>Créditos</h1>
 
-      <p>
-        Diferentes Facultades, Dependencias y Unidades Académico Administrativas de las Sedes Andinas (Bogotá, Medellín,
-        Manizales, Palmira y De La Paz) y de las Sedes de Presencia Nacional (Amazonía, Caribe, Orinoquia y Tumaco) de
-        la Universidad Nacional de Colombia trabajan en Cuenco de manera independiente e inarticulada. Por esta razón,
-        el Instituto Nacional es el llamado a liderar y promover las acciones pertinentes en las áreas de investigación,
-        innovación y política educativa, bajo una propuesta integrativa y desde el modelo intersedes. Por ende, le
-        concierne proyectar en el medio externo la labor institucional en las áreas del conocimiento que enuncia, siendo
-        estratégicas para el desarrollo de las regiones, tanto de las de influencia actual de la universidad como de los
-        territorios nacionales en general.
-      </p>
+      <p>Página en construcción.</p>
 
-      <h2>Créditos Cuenco</h2>
-
-      <p>Investigación</p>
+      <!-- <p>Investigación</p>
       <p>Consolidación de datos</p>
       <p>...</p>
 
       <h2>Diseño y desarrollo</h2>
-      <p>Juan Camilo González y Antonia Bustamante - <a href="https://enflujo.com/">Laboratorio EnFlujo</a></p>
+      <p>Juan Camilo González y Antonia Bustamante - <a href="https://enflujo.com/">Laboratorio EnFlujo</a></p> -->
     </div>
   </main>
 </template>
